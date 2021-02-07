13c79f132c24f46a82b7eab6e9a052f7965864e5
Chain Singh <chainrmsa3025@gmail.com>
2021-02-08 00:54:02 +0530

    Intent is add to switch b/w to activity

--- app/src/main/java/com/example/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         btnSeeBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this,AllBookActivity.class);
+                startActivity(intent);
             }
         });
     }

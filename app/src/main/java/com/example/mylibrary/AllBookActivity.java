b3a8ea7cc336d6c70951f738c0a8cd903f3df1d8
Chain Singh <chainrmsa3025@gmail.com>
2021-02-08 01:31:21 +0530

    recycler view class created

--- app/src/main/java/com/example/mylibrary/AllBookActivity.java
+++ app/src/main/java/com/example/mylibrary/AllBookActivity.java
@@ -1,14 +1,26 @@
 package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AllBookActivity extends AppCompatActivity {
-
+private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_book);
-    }
+
+        recyclerView = (RecyclerView)findViewById(R.id.booksRecyclerview);
+
+        ArrayList<String> books = new ArrayList<>();
+        books.addAll(Arrays.asList(new String[]{"DAA", "CN", "DSA", "OS"}));
+
+        }
 }
---
+++ app/src/main/java/com/example/mylibrary/RecyclerViewAdapter.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.ViewHolder {
+    public RecyclerViewAdapter(@NonNull View itemView) {
+        super(itemView);
+    }
+}

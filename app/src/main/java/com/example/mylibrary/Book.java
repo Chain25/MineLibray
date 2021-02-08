2fc1d4f1f5b53ef4588736a7dd3b4d24f727196c
Chain Singh <chainrmsa3025@gmail.com>
2021-02-08 10:17:54 +0530

    adapter created but view is not

---
+++ app/src/main/java/com/example/mylibrary/Book.java
@@ -0,0 +1,14 @@
+package com.example.mylibrary;
+
+public class Book {
+
+    private String name;
+    private String author;
+    private int pages;
+    private String imageUrl;
+    private String description;
+
+    Book(){
+
+    }
+}
--- app/src/main/java/com/example/mylibrary/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/mylibrary/RecyclerViewAdapter.java
@@ -1,12 +1,43 @@
 package com.example.mylibrary;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerViewAdapter extends RecyclerView.ViewHolder {
-    public RecyclerViewAdapter(@NonNull View itemView) {
-        super(itemView);
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView bookImage;
+        private TextView bookName;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bookImage =(ImageView) itemView.findViewById(R.id.bookImage);
+            bookName = (TextView) itemView.findViewById(R.id.bookName);
+        }
     }
 }
+
